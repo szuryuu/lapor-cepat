930c90d0ed71d6c64b32592a2f9a1ac5466f7f96
szuryuu <ilhamdzaky2007@gmail.com>
2026-03-14 23:42:54 +0700

    chore: enable nuxt 4 compatibility mode

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,9 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: "2025-07-15",
+  future: {
+    compatibilityVersion: 4,
+  },
   devtools: { enabled: true },
   modules: ["@nuxtjs/seo", "@nuxtjs/tailwindcss"],
   app: {
@@ -10,7 +13,8 @@ export default defineNuxtConfig({
       meta: [
         { name: 'theme-color', content: '#0f172a' },
         { name: 'apple-mobile-web-app-capable', content: 'yes' },
-        { name: 'apple-mobile-web-app-status-bar-style', content: 'black-translucent' }
+        { name: 'apple-mobile-web-app-status-bar-style', content: 'black-translucent' },
+        { name: 'dicoding:email', content: 'ilhamdzaky2007@gmail.com' }
       ],
       link: [
         { rel: 'manifest', href: '/manifest.json' }
@@ -18,7 +22,7 @@ export default defineNuxtConfig({
     }
   },
   runtimeConfig: {
-    bpbdPin: process.env.NUXT_PUBLIC_BPBD_PIN || "",
+    bpbdPin: process.env.BPBD_PIN || "",
     groqApiKey: process.env.GROQ_API_KEY || "",
     geminiApiKey: process.env.GEMINI_API_KEY || "",
     firebaseServiceAccount: process.env.FIREBASE_SERVICE_ACCOUNT || "",
